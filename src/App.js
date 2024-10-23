06a491b88deaa7a80d5747a5aa9da04dc0ef6ac9
fran14cruz <fran14.ich@gmail.com>
2024-10-23 17:26:31 -0500

    Updating App state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
+import './App.css';
 import { plants } from './plants';
 
-const App = () => {
-  return (
-    <CardList plants={plants} />
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      plants,
+      searchfield: ''
+    }
+  }
+
+  // handle event
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+    console.log(event.target.value)
+  }
+
+  render() {
+    const filteredPlants = this.state.plants.filter(plant => {
+      return plant.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+      <div className='tc'>
+        <h1>PlantFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList plants={filteredPlants} />
+      </div>
+    );    
+  }
 }
 
 export default App;
--- src/plants.js
+++ src/plants.js
@@ -13,8 +13,38 @@ export const plants = [
   },
   {
     id: 3,
+    name: 'Lily',
+    username: 'Fran',
+    email: 'something@gmail.com'
+  },
+  {
+    id: 4,
     name: 'Rose',
     username: 'Fran',
     email: 'something@gmail.com'
   },
+  {
+    id: 5,
+    name: 'Other',
+    username: 'Fran',
+    email: 'something@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Rose',
+    username: 'Fran',
+    email: 'something@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Other',
+    username: 'Fran',
+    email: 'something@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Buganvilla',
+    username: 'Fran',
+    email: 'something@gmail.com'
+  }
 ]
