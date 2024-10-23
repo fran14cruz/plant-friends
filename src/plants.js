4d2f32334dac99114f6f2f863e3c0c9c08484f71
fran14cruz <fran14.ich@gmail.com>
2024-10-23 15:41:38 -0500

    Adding App.js component

---
+++ src/plants.js
@@ -0,0 +1,20 @@
+export const plants = [
+  {
+    id: 1,
+    name: 'Rose',
+    username: 'Fran',
+    email: 'something@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Rose',
+    username: 'Fran',
+    email: 'something@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Rose',
+    username: 'Fran',
+    email: 'something@gmail.com'
+  },
+]
