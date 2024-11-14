89ee3915f91e40a81efbf0a888457c2d7cf2c427
fran14cruz <fran14.ich@gmail.com>
2024-11-14 18:27:44 -0500

    migrate to hooks

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      plants: [],
-      searchfield: ''
-    }
-  }
+function App() {
+  const [plants, setPlants] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
 
-  componentDidMount() {
+  useEffect(() => { // is called every time the app renders
     fetch('https://jsonplaceholder.typicode.com/users') // make an HTTP request
-      .then(response => {
-        return response.json(); // parse JSON to JS object
-      })
-      .then(users => {
-        this.setState({ plants: users})
-      })
+      .then(response => response.json()) // parse JSON to JS object
+      .then(users => setPlants(users))
+      .catch(err => console.log(err))
+  }, []) // only run useEffect if [] has changed, and it will never change. So this hook
+  // will only run once, i.e. only the first time the component is rendered.
+  // this is a shortcut for componentDidMount lifecycle method.
 
+  // handle search event
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   }
 
-  // handle event
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-    console.log(event.target.value)
-  }
-
-  render() {
-    // filter logic
-    const filteredPlants = this.state.plants.filter(plant => {
-      return plant.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
-    if (this.state.plants.length === 0) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        <div className='tc'>
-          <h1 className='f-headline'>Random Friends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <CardList plants={filteredPlants} />
-        </div>
-      );
-    }  
+  // filter logic
+  const filteredPlants = plants.filter(plant => {
+    return plant.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
+  if (plants.length === 0) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className='tc'>
+        <h1 className='f-headline'>Random Friends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <CardList plants={filteredPlants} />
+      </div>
+    );
   }
 }
 
